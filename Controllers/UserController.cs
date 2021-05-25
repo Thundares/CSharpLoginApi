0a59683c4a4f442c12a7a131835c0cf3ba757962
Kenzo Mippo <pedrac.u@gmail.com>
2021-05-25 17:25:32 -0300

    Added controller of user and models for login and register

---
+++ Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using LoginApi.Models.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult login(LoginViewModels loginViewModel) 
+        {
+            return Ok(loginViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult register(RegisterViewModels loginViewModel)
+        {
+            return Created("", loginViewModel);
+        }
+    }
+}
---
+++ Models/User/RegisterViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginApi.Models.User
+{
+    public class RegisterViewModels
+    {
+        [Required(ErrorMessage = "O campo Nome é necessário.")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "O campo E-mail é necessário.")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "O campo Login é necessário.")]
+        public string login { get; set; }
+
+        [Required(ErrorMessage = "O campo Senha é necessário.")]
+        public string password { get; set; }
+    }
+}
